420acf89fd5e3b8670eeb05372ef665f5d4b13e9
Oscar Hernandez <os.hf024@gmail.com>
2021-10-14 23:02:06 -0500

    build: First setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import app from './server.js';
+
+// Starting the server
+app
+  .listen(app.get('port'), () => {
+    console.log(`Server listening at http://localhost:${app.get('port')}`);
+  })
+  .on('error', (error) => {
+    console.error(error.stack);
+  });
---
+++ src/server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import morgan from 'morgan';
+import responseTime from 'response-time';
+
+// Initializations
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+
+// Middlewares
+app.use(responseTime());
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World! :D');
+});
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({ message: 'Error 404!'});
+});
+
+// Error handler
+app.use((req, res) => {
+  res.status(500).json({ message: 'Server error!'});
+});
+
+export default app;
