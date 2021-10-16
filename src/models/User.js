dde4f97e3ed305060b3cf24a97dd4172d23451fb
Oscar Hernandez <os.hf024@gmail.com>
2021-10-16 13:25:35 -0500

    build: Add User model

---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+import { model, Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+const User = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
+
+User.methods.encryptPassword = async (password) => {
+  return await bcrypt.hash(password, 10);
+};
+
+User.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export default model('users', User);
